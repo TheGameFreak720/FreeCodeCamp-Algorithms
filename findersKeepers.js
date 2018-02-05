9a254e356632a0cfe6fb8f52c402eb56d1e0719b
Luis M Alvarez Pacheco <luis.alvarez@commithub.com>
2018-02-05 08:30:10 -0500

    Add files via upload
    Finished intermediate algos

---
+++ findersKeepers.js
@@ -0,0 +1,19 @@
+function findElement(arr, func) {
+ 
+  var num;
+
+  for (var a = 0; a < arr.length; a++) {
+    if (func(arr[a])) {
+      num = arr[a];
+      return num;
+    }
+  }
+
+  return num;
+}
+
+findElement([1, 2, 3, 4], function(num) {
+  return num % 2 === 0;
+});
+
+findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
---
+++ sumAllPrimes.js
@@ -0,0 +1,30 @@
+
+function sumPrimes(num) {
+  var res = 0;
+
+  function getPrimes(max) {
+    var sieve = [];
+    var primes = [];
+    
+    for (var i = 2; i <= max; ++i) {
+      if (!sieve[i]) {
+        primes.push(i);
+        for (var j = i << 1; j <= max; j += i) {
+          sieve[j] = true;
+        }
+      }
+    }
+
+    return primes;
+  }
+
+  var primes = getPrimes(num);
+  for (var p = 0; p < primes.length; p++) {
+    res += primes[p];
+  }
+
+  return res;
+}
+
+
+sumFibs(4);
